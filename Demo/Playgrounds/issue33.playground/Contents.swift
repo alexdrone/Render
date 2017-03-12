d4bef2271f60d519749864e397398c96958addd6
alexdrone Alex Usbergo <alexakadrone@gmail.com>
2017-03-12 08:52:56 -0700

    better support for containers

--- Demo/Playgrounds/issue33.playground/Contents.swift
+++ Demo/Playgrounds/issue33.playground/Contents.swift
@@ -1,6 +1,8 @@
 import PlaygroundSupport
-
 import Render
+
+//33: Updating state after using NilNode makes the order of items reversed
+// Fixed.
 
 PlaygroundPage.current.needsIndefiniteExecution = true
 
@@ -10,24 +12,17 @@ struct AppState: StateType {
 
 class MyView: ComponentView<AppState> {
   override func construct(state: AppState?, size: CGSize) -> NodeType {
-
     let bar = Node<UILabel>() { label, layout, size in
       label.text = "bar"
-
     }
-
-    return Node<UIView> { (view, layout, size) in
-
-      }
-      .add(children: [
+    return Node<UIView>().add(children: [
         Node<UILabel>() { label, layout, size in
           label.text = "foo"
         },
         state!.isOn ? bar as NodeType : NilNode()
-      ])
+    ])
   }
 }
-
 
 let container = UIView(frame: CGRect(x: 0, y: 0, width: 320, height: 568))
 container.backgroundColor = .white
@@ -44,6 +39,5 @@ view.state?.isOn = true
 view.render(in: container.frame.size)
 
 container
-
 
 PlaygroundPage.current.liveView = container
