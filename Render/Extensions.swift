b87b6af6e1fda30bedab05be891072f2ed22c75f
Brendan Kilfoil <brendan.kilfoil@finger-ink.com>
2017-05-16 23:59:16 +1200

    Allow arbitrary defaults for getBool
    Previously different defaults were being used for each property:
    
    - isAnimatable: true
    - hasNode: false
    - isNewlyCreated: false
    
    The latest update changed this behaviour to default them all to true.
    This could break rendering in some cases.

--- Render/Extensions.swift
+++ Render/Extensions.swift
@@ -29,24 +29,24 @@ fileprivate var handleNewlyCreated: UInt8 = 0
 public extension UIView {
 
   public var isAnimatable: Bool {
-    get { return getBool(&handleAnimatable, self) }
+    get { return getBool(&handleAnimatable, self, defaultIfNil: true) }
     set { setBool(&handleAnimatable, self, newValue) }
   }
 
   internal var hasNode: Bool {
-    get { return getBool(&handleHasNode, self) }
+    get { return getBool(&handleHasNode, self, defaultIfNil: false) }
     set { setBool(&handleHasNode, self, newValue) }
   }
 
   internal var isNewlyCreated: Bool {
-    get { return getBool(&handleNewlyCreated, self) }
+    get { return getBool(&handleNewlyCreated, self, defaultIfNil: false) }
     set { setBool(&handleNewlyCreated, self, newValue) }
   }
 }
 
 
-fileprivate func getBool(_ handle: UnsafeRawPointer!, _ object: UIView) -> Bool {
-  return (objc_getAssociatedObject(object, handle) as? NSNumber)?.boolValue ?? true
+fileprivate func getBool(_ handle: UnsafeRawPointer!, _ object: UIView, defaultIfNil: Bool) -> Bool {
+  return (objc_getAssociatedObject(object, handle) as? NSNumber)?.boolValue ?? defaultIfNil
 }
 
 fileprivate func setBool(_ handle: UnsafeRawPointer!, _ object: UIView, _ value: Bool) {
