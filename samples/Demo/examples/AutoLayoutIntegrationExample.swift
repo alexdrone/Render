f84fb74b74b33b1350fd395bcd17b3208ac30a60
alexdrone Alex Usbergo <alexakadrone@gmail.com>
2017-08-20 14:14:12 +0200

    AutoLayoutComponentAnchorView

--- samples/Demo/examples/AutoLayoutIntegrationExample.swift
+++ samples/Demo/examples/AutoLayoutIntegrationExample.swift
@@ -6,20 +6,14 @@ class AutoLayoutIntegrationExample: ViewController  {
   override func loadView() {
     super.loadView()
 
-    let avatar = AutoLayoutNestedComponentExample()
+    let avatar = AutoLayoutComponentAnchorView(component: AutoLayoutNestedComponentExample())
     view.addSubview(avatar)
-    avatar.translatesAutoresizingMaskIntoConstraints = false
-
-    // Render ComponentViews implement 'intrinsicContentSize'.
-    // To constrain them to a specific size you have to set  use 'referenceSize' proprerty.
-    // N.B: You can do this also in 'viewDidLayoutSubviews' if the size of the component is not
-    // static (e.g. could depend on another view layed out with autolayout).
-    avatar.referenceSize = { _ in return CGSize(width: 50, height: 50)}
-    avatar.update()
 
     let cts = [
       avatar.centerXAnchor.constraint(equalTo: view.centerXAnchor),
       avatar.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+      avatar.widthAnchor.constraint(equalToConstant: 50),
+      avatar.heightAnchor.constraint(equalToConstant: 50),
     ]
     view.addConstraints(cts)
   }
