b54f79ccf047937ef494b462b92d5ce3d51eefca
alexdrone Alex Usbergo <alexakadrone@gmail.com>
2017-10-23 14:30:36 +0200

    improved demo

--- samples/Neutrino/src/Padded.swift
+++ samples/Neutrino/src/Padded.swift
@@ -5,7 +5,6 @@ import RenderNeutrino
 public struct PaddedLabel {
 
   public class Props: UIPropsProtocol {
-    public var isImportant: Bool = true
     public var text: String = "A neutrino (/nuːˈtriːnoʊ/ or /njuːˈtriːnoʊ/) (denoted by the Greek letter ν) is a fermion (an elementary particle with half-integer spin) that interacts only via the weak subatomic force and gravity. The mass of the neutrino is much smaller than that of the other known elementary particles."
     required public init() { }
   }
@@ -24,45 +23,54 @@ public struct PaddedLabel {
       let state = self.state
 
       let root = UINode<UIScrollView> { layout in
-        layout.set(\UIScrollView.backgroundColor, value: props.isImportant ? .orange : .gray)
-        layout.set(\UIScrollView.yoga.padding, value: 50)
+        layout.set(\UIScrollView.backgroundColor, value: Color.black)
+        layout.set(\UIScrollView.yoga.padding, value: 8)
         layout.set(\UIScrollView.yoga.alignSelf, value: .center)
         layout.set(\UIScrollView.yoga.width, value: layout.canvasSize.width)
-        layout.set(\UIScrollView.yoga.maxHeight, value: context.canvasView?.bounds.size.height ?? 0)
+        layout.set(\UIScrollView.yoga.height, value: context.canvasView?.bounds.size.height ?? 0)
       }
 
       let label = UINode<UILabel>() { layout in
-        layout.set(\UILabel.numberOfLines, value: 100)
-        layout.set(\UILabel.textColor, value: props.isImportant ? .white : .black)
-        layout.set(\UILabel.font, value: UIFont.boldSystemFont(ofSize: 14))
-
-        var text = Array(0...state.count).reduce("", { str, _ in str + props.text })
-        layout.set(\UILabel.text, value: text)
+        layout.set(\UILabel.numberOfLines, value: 0)
+        layout.set(\UILabel.textColor, value: Color.white)
+        layout.set(\UILabel.text, value: props.text)
+        layout.set(\UILabel.font,
+                   value: UIFont.systemFont(ofSize: 12 + CGFloat(state.count)/2,
+                                            weight: UIFont.Weight.light))
       }
 
       let count = UINode<UILabel>() { layout in
         layout.set(\UILabel.text, value: "\(state.count)")
-        layout.set(\UILabel.backgroundColor, value: .clear)
+        layout.set(\UILabel.backgroundColor, value: Color.red)
         layout.set(\UILabel.textColor, value: .white)
-        layout.set(\UILabel.yoga.padding, value: CGFloat(state.count*2))
-        layout.node.bindView(target: self, keyPath: \PaddedLabel.Component.counterLabel)
+        layout.set(\UILabel.font, value: Typography.mediumBold)
+        layout.set(\UILabel.textAlignment, value: .center)
+        layout.set(\UILabel.yoga.width, value: 32)
+        layout.set(\UILabel.yoga.height, value: 32)
+        layout.set(\UILabel.layer.cornerRadius, value: 16)
+        layout.set(\UILabel.clipsToBounds, value: true)
+        layout.set(\UILabel.yoga.alignSelf, value: .center)
+        layout.set(\UILabel.yoga.margin, value: 16)
       }
 
-      let button = UINode<UIButton>(reuseIdentifier: "increase") { layout in
-        layout.set(\UIButton.yoga.marginTop, value: 10)
-        layout.set(\UIButton.backgroundColor, value: #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1))
+      let button = UINode<UIButton>(reuseIdentifier: "Increase") { layout in
+        layout.set(\UIButton.yoga.margin, value: 16)
+        layout.set(\UIButton.backgroundColor, value: Color.green)
+
         let animator = UIViewPropertyAnimator(duration: 1, dampingRatio: 1, animations: nil)
-        layout.set(\UIButton.alpha, animator: animator, value: randomCGFloat())
+        layout.set(\UIButton.alpha, animator: animator, value: 0.1 + (CGFloat(state.count)+1)/10)
         layout.view.onTap { [weak self] _ in
-          self?.state.count += 1
-          let animator = UIViewPropertyAnimator(duration: 0.5, dampingRatio: 1, animations: nil)
-          self?.setNeedsRender(layoutAnimator: animator)
+          guard let `self` = self else { return }
+          self.state.count = self.state.count == 9 ? 0 : self.state.count + 1
+          // Render animated.
+          let animator = UIViewPropertyAnimator(duration: 1, dampingRatio: 0.7, animations: nil)
+          self.setNeedsRender(layoutAnimator: animator)
         }
         layout.view.setTitle("INCREASE", for: .normal)
         layout.view.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
       }
 
-      root.children([button, label, count])
+      root.children([button, count, label])
       return root
     }
   }
---
+++ samples/Neutrino/src/Style.swift
@@ -0,0 +1,66 @@
+import UIKit
+
+struct Color {
+  static let white = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+  static let darkerBlack: UIColor = #colorLiteral(red: 0.07600152266, green: 0.08952714743, blue: 0.1140301166, alpha: 1)
+  static let black: UIColor = #colorLiteral(red: 0.1870646477, green: 0.2185702622, blue: 0.2767287493, alpha: 1)
+  static let green: UIColor = #colorLiteral(red: 0.5828176141, green: 0.8645806313, blue: 0.812251389, alpha: 1)
+  static let red: UIColor = #colorLiteral(red: 0.8645806313, green: 0.4114574932, blue: 0.3395885195, alpha: 1)
+  static let darkerRed: UIColor = #colorLiteral(red: 0.6853343588, green: 0.3291431345, blue: 0.2744288248, alpha: 1)
+  static let darkerGreen: UIColor = #colorLiteral(red: 0.1449353099, green: 0.4921078086, blue: 0.4273921251, alpha: 1)
+  static let gray: UIColor = #colorLiteral(red: 0.9998916984, green: 1, blue: 0.9998809695, alpha: 1)
+}
+
+struct Typography {
+  static let big = UIFont.systemFont(ofSize: 36.0, weight: UIFont.Weight.bold)
+  static let mediumBold = UIFont.systemFont(ofSize: 18.0, weight: UIFont.Weight.bold)
+  static let small = UIFont.systemFont(ofSize: 12.0, weight: UIFont.Weight.regular)
+  static let smallBold = UIFont.systemFont(ofSize: 12.0, weight: UIFont.Weight.bold)
+  static let smallLight = UIFont.systemFont(ofSize: 12.0, weight: UIFont.Weight.light)
+  static let extraSmallLight = UIFont.systemFont(ofSize: 10.0, weight: UIFont.Weight.light)
+}
+
+extension UIView {
+  var isShimmering: Bool {
+    return layer.mask != nil
+  }
+
+  func startShimmering() {
+    stopShimmering()
+    let light: CGColor = UIColor(white: CGFloat(0), alpha: CGFloat(0.1)).cgColor
+    let dark: CGColor = UIColor.black.cgColor
+    let gradient = CAGradientLayer()
+    gradient.colors = [dark, light, dark]
+    gradient.frame = CGRect(x: CGFloat(-bounds.size.width),
+                            y: CGFloat(0),
+                            width: CGFloat(3 * bounds.size.width),
+                            height: CGFloat(bounds.size.height))
+    gradient.startPoint = CGPoint(x: CGFloat(0.0), y: CGFloat(0.5))
+    gradient.endPoint = CGPoint(x: CGFloat(1.0), y: CGFloat(0.525))
+    // slightly slanted forward
+    gradient.locations = [0.4, 0.5, 0.6]
+    layer.mask = gradient
+    let animation = CABasicAnimation(keyPath: "locations")
+    animation.fromValue = [0.0, 0.1, 0.2]
+    animation.toValue = [0.8, 0.9, 1.0]
+    animation.duration = 1.5
+    animation.repeatCount = Float.infinity
+    gradient.add(animation, forKey: "shimmer")
+  }
+  func stopShimmering() {
+    layer.mask = nil
+  }
+}
+
+extension CGFloat {
+  static func random() -> CGFloat {
+    return CGFloat(arc4random()) / CGFloat(UInt32.max)
+  }
+}
+
+extension UIColor {
+  static var random: UIColor {
+    return UIColor(red: .random(), green: .random(), blue: .random(), alpha: 1.0)
+  }
+}
+
