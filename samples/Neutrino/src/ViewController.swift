e602638f01b1c13a063fd9299ad49638d669f2d7
alexdrone Alex Usbergo <alexakadrone@gmail.com>
2017-10-24 13:17:33 +0200

    ios10 compliancy

--- samples/Neutrino/src/ViewController.swift
+++ samples/Neutrino/src/ViewController.swift
@@ -13,12 +13,22 @@ class ViewController: UIViewController {
 
     safeAreaView.translatesAutoresizingMaskIntoConstraints = false
     safeAreaView.backgroundColor = UIColor.blue
-    let constraints = [
-      safeAreaView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-      safeAreaView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-      safeAreaView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
-      safeAreaView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
-    ]
+    var constraints: [NSLayoutConstraint] = []
+    if #available(iOS 11.0, *) {
+      constraints = [
+        safeAreaView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+        safeAreaView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        safeAreaView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+        safeAreaView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+      ]
+    } else {
+      constraints = [
+        safeAreaView.topAnchor.constraint(equalTo: view.topAnchor),
+        safeAreaView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        safeAreaView.leftAnchor.constraint(equalTo: view.leftAnchor),
+        safeAreaView.rightAnchor.constraint(equalTo: view.rightAnchor),
+      ]
+    }
     view.addSubview(safeAreaView)
     NSLayoutConstraint.activate(constraints)
     component.setCanvas(view: safeAreaView)
