7103a1ffbd8f0e32b2dfc7d09e9de17cb46d2dae
alexdrone Alex Usbergo <alexakadrone@gmail.com>
2017-11-03 14:58:58 +0100

    moved deps

---
+++ Neutrino/deps/keycommands/KeyCommands.swift
@@ -0,0 +1,112 @@
+import UIKit
+
+// MARK: - KeyCommands
+// forked from: Augustyniak/KeyCommands by Rafal Augustyniak
+
+#if (arch(i386) || arch(x86_64)) && (os(iOS) || os(tvOS))
+  public typealias KeyModifierFlags  = UIKeyModifierFlags
+
+  struct KeyActionableCommand {
+    fileprivate let keyCommand: UIKeyCommand
+    fileprivate let actionBlock: () -> ()
+
+    func matches(_ input: String, modifierFlags: UIKeyModifierFlags) -> Bool {
+      return keyCommand.input == input && keyCommand.modifierFlags == modifierFlags
+    }
+  }
+
+  func == (lhs: KeyActionableCommand, rhs: KeyActionableCommand) -> Bool {
+    return lhs.keyCommand.input == rhs.keyCommand.input
+      && lhs.keyCommand.modifierFlags == rhs.keyCommand.modifierFlags
+  }
+
+  public enum KeyCommands {
+    private static var __once: () = {
+      exchangeImplementations(class: UIApplication.self,
+                              originalSelector: #selector(getter: UIResponder.keyCommands),
+                              swizzledSelector: #selector(UIApplication.KYC_keyCommands));
+    }()
+    fileprivate struct Static {
+      static var token: Int = 0
+    }
+
+    struct KeyCommandsRegister {
+      static var sharedInstance = KeyCommandsRegister()
+      fileprivate var actionableKeyCommands = [KeyActionableCommand]()
+    }
+
+    public static func register(input: String,
+                                modifierFlags: KeyModifierFlags,
+                                action: @escaping () -> ()) {
+      _ = KeyCommands.__once
+      let keyCommand = UIKeyCommand(input: input,
+                                    modifierFlags: modifierFlags,
+                                    action: #selector(UIApplication.KYC_handleKeyCommand(_:)),
+                                    discoverabilityTitle: "")
+      let actionableKeyCommand = KeyActionableCommand(keyCommand: keyCommand, actionBlock: action)
+      let index = KeyCommandsRegister.sharedInstance.actionableKeyCommands.index(
+        where: { return $0 == actionableKeyCommand })
+      if let index = index {
+        KeyCommandsRegister.sharedInstance.actionableKeyCommands.remove(at: index)
+      }
+      KeyCommandsRegister.sharedInstance.actionableKeyCommands.append(actionableKeyCommand)
+    }
+
+    public static func unregister(input: String, modifierFlags: KeyModifierFlags) {
+      let index = KeyCommandsRegister.sharedInstance.actionableKeyCommands.index(
+        where: { return $0.matches(input, modifierFlags: modifierFlags) })
+      if let index = index {
+        KeyCommandsRegister.sharedInstance.actionableKeyCommands.remove(at: index)
+      }
+    }
+  }
+
+  extension UIApplication {
+    @objc dynamic func KYC_keyCommands() -> [UIKeyCommand] {
+      return KeyCommands.KeyCommandsRegister.sharedInstance.actionableKeyCommands.map({
+        return $0.keyCommand
+      })
+    }
+
+    @objc func KYC_handleKeyCommand(_ keyCommand: UIKeyCommand) {
+      for command in KeyCommands.KeyCommandsRegister.sharedInstance.actionableKeyCommands {
+        if command.matches(keyCommand.input!, modifierFlags: keyCommand.modifierFlags) {
+          command.actionBlock()
+        }
+      }
+    }
+  }
+
+  func exchangeImplementations(class classs: AnyClass,
+                               originalSelector: Selector,
+                               swizzledSelector: Selector ){
+    let originalMethod = class_getInstanceMethod(classs, originalSelector)
+    let originalMethodImplementation = method_getImplementation(originalMethod!)
+    let originalMethodTypeEncoding = method_getTypeEncoding(originalMethod!)
+    let swizzledMethod = class_getInstanceMethod(classs, swizzledSelector)
+    let swizzledMethodImplementation = method_getImplementation(swizzledMethod!)
+    let swizzledMethodTypeEncoding = method_getTypeEncoding(swizzledMethod!)
+    let didAddMethod = class_addMethod(classs,
+                                       originalSelector,
+                                       swizzledMethodImplementation,
+                                       swizzledMethodTypeEncoding)
+    if didAddMethod {
+      class_replaceMethod(classs,
+                          swizzledSelector,
+                          originalMethodImplementation,
+                          originalMethodTypeEncoding)
+    } else {
+      method_exchangeImplementations(originalMethod!, swizzledMethod!)
+    }
+  }
+
+#else
+  public typealias KeyModifierFlags = Int
+
+  public enum KeyCommands {
+    public static func register(input: String,
+                                modifierFlags: KeyModifierFlags,
+                                action: () -> ()) {}
+    public static func unregister(input: String, modifierFlags: KeyModifierFlags) {}
+  }
+#endif
