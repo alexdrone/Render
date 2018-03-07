9120650d5e048228e391da9ca5f2093870441773
Alex Usbergo <alexusbergo@alexusbergo-macpro.roam.corp.google.com>
2018-03-07 16:35:32 +0100

    navigation bar example

---
+++ samples/catalog/src/scenes/viewcontrollers/CustomNavigationBarViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CustomNavigationBarController.swift
+//  RenderNeutrinoDemo
+//
+//  Created by Alex Usbergo on 3/7/18.
+//  Copyright © 2018 Alex Usbergo. All rights reserved.
+//
+
+import Foundation
